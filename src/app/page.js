70a4c3c37615aed31ff0dbab26bf32ac127916b8
younusraza909 <younusraza909@gmail.com>
2023-07-21 10:13:25 +0500

    making a simple css board

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,26 @@
 export default function Home() {
+  let totalGridSize = 20;
+
+  function renderBoard() {
+    let cellArray = [];
+
+    for (let row = 0; row < totalGridSize; row++) {
+      for (let col = 0; col < totalGridSize; col++) {
+        let cell = <div className='cell'></div>;
+
+        cellArray.push(cell);
+      }
+    }
+
+    return cellArray;
+  }
+
   return (
-    <main>
-      <p>Hello World</p>
+    <main className='main'>
+      <div className='score'>
+        Score : <span>30</span>
+      </div>
+      <div className='board'>{renderBoard()}</div>
     </main>
   );
 }
