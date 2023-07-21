194259d32cd6f110f733a4c1eb6342853b2659af
younusraza909 <younusraza909@gmail.com>
2023-07-21 10:22:28 +0500

    render food and snake

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,47 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
   let totalGridSize = 20;
+
+  // Game State
+  const [score, setScore] = useState(0);
+  const [food, setFood] = useState({
+    x: 5,
+    y: 5,
+  });
+  const [snake, setSnake] = useState([
+    {
+      x: totalGridSize / 2,
+      y: totalGridSize / 2,
+    },
+    {
+      x: totalGridSize / 2,
+      y: totalGridSize / 2 + 1,
+    },
+  ]);
 
   function renderBoard() {
     let cellArray = [];
 
     for (let row = 0; row < totalGridSize; row++) {
       for (let col = 0; col < totalGridSize; col++) {
-        let cell = <div className='cell'></div>;
+        let classes = "cell";
+
+        let isFood = food.x === row && food.y === col;
+
+        let isSnake = snake.some((ele) => ele.x === row && ele.y === col);
+
+        if (isFood) {
+          classes = `${classes} food`;
+        }
+
+        if (isSnake) {
+          classes = `${classes} snake food`;
+        }
+
+        let cell = <div key={`${row}-${col}`} className={classes}></div>;
 
         cellArray.push(cell);
       }
@@ -18,7 +53,7 @@ export default function Home() {
   return (
     <main className='main'>
       <div className='score'>
-        Score : <span>30</span>
+        Score : <span>{score}</span>
       </div>
       <div className='board'>{renderBoard()}</div>
     </main>
